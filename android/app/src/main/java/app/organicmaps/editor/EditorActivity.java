2729d9d924d8339df03b682347f807dcb40b10f2
DinoCO868 <dinoaybar@gmail.com>
2026-09-01 13:31:22 -0300

    Disable public OSM editor in AeroGuard

--- android/app/src/main/java/app/organicmaps/editor/EditorActivity.java
+++ android/app/src/main/java/app/organicmaps/editor/EditorActivity.java
@@ -2,13 +2,27 @@ package app.organicmaps.editor;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import app.organicmaps.AeroGuardPolicy;
 import app.organicmaps.base.BaseMwmFragmentActivity;
 
 public class EditorActivity extends BaseMwmFragmentActivity
 {
   public static final String EXTRA_NEW_OBJECT = "ExtraNewMapObject";
+
+  @Override
+  protected void onSafeCreate(@Nullable Bundle savedInstanceState)
+  {
+    if (!AeroGuardPolicy.OSM_EDITING_ENABLED)
+    {
+      finish();
+      return;
+    }
+    super.onSafeCreate(savedInstanceState);
+  }
 
   @Override
   protected Class<? extends Fragment> getFragmentClass()
@@ -18,6 +32,9 @@ public class EditorActivity extends BaseMwmFragmentActivity
 
   public static void start(@NonNull Activity activity)
   {
+    if (!AeroGuardPolicy.OSM_EDITING_ENABLED)
+      return;
+
     final Intent intent = new Intent(activity, EditorActivity.class);
     activity.startActivity(intent);
   }
