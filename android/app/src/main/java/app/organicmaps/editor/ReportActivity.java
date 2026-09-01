c151c85a0b3b8aafeecceafa435d3406f770dbf9
DinoCO868 <dinoaybar@gmail.com>
2026-09-01 14:29:53 -0300

    Disable OSM notes reporting in AeroGuard

--- android/app/src/main/java/app/organicmaps/editor/ReportActivity.java
+++ android/app/src/main/java/app/organicmaps/editor/ReportActivity.java
@@ -1,10 +1,24 @@
 package app.organicmaps.editor;
 
+import android.os.Bundle;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import app.organicmaps.AeroGuardPolicy;
 import app.organicmaps.base.BaseMwmFragmentActivity;
 
 public class ReportActivity extends BaseMwmFragmentActivity
 {
+  @Override
+  protected void onSafeCreate(@Nullable Bundle savedInstanceState)
+  {
+    if (!AeroGuardPolicy.OSM_EDITING_ENABLED)
+    {
+      finish();
+      return;
+    }
+    super.onSafeCreate(savedInstanceState);
+  }
+
   @Override
   protected Class<? extends Fragment> getFragmentClass()
   {
